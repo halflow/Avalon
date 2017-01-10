15cac9cad45d9232267b5920d22492cff738667f
halflow <halflow@126.com>
2017-01-10 16:22:57 +0800

    pc20170110

--- py/tcpserver/s.py
+++ py/tcpserver/s.py
@@ -11,7 +11,7 @@ addr = (host,port)
 class Servers(SRH):  
     def handle(self):  
         print('got connection from ',self.client_address)
-        self.wfile.write('connection %s:%s at %s succeed!' % (host,port,ctime()))  
+        #self.wfile.write('connection %s:%s at %s succeed!' % (host,port,ctime()))  
         while True:  
             data = self.request.recv(1024)  
             if not data:   

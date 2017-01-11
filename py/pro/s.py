b686376b1d706e7ce136703d1aa261d7c3351cfa
halflow <halflow@126.com>
2017-01-11 18:39:34 +0800

    pc20160111

--- py/pro/s.py
+++ py/pro/s.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
 #创建SocketServerTCP服务器：  
 import socketserver
+import queque
 from socketserver import StreamRequestHandler as SRH  
 from time import ctime  
+
+q=queuqe.queue(20)
   
 host = '192.168.1.108'  
 port = 9999  
 addr = (host,port)  
+
+#写入
+def bufwrite(message)
+    q.put(message)
   
 class Servers(SRH):  
     def handle(self):  
         print('got connection from ',self.client_address)
-        self.wfile.write('connection %s:%s at %s succeed!' % (host,port,ctime()))  
+        #self.wfile.write('connection %s:%s at %s succeed!' % (host,port,ctime()))  
         while True:  
             data = self.request.recv(1024)  
             if not data:   
                 break  
-            print(data)  
-            print("RECV from ", self.client_address[0])  
-            self.request.send(data)  
+            bufwrite(data)  
+            print("RECV from ", self.client_address[0]) 
+            if not q.empty():        			
+                self.request.send(q.get)  
 print('server is running....')  
 server = socketserver.ThreadingTCPServer(addr,Servers)  
 server.serve_forever()

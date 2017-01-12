317148104b163446d026fc827816d41f09844fe8
halflow <halflow@126.com>
2017-01-12 18:29:07 +0800

    pc20170112

--- py/pro/s1.py
+++ py/pro/s1.py
@@ -42,7 +42,7 @@ class Servers(SRH):
             if data:
                 l1=(self.client_address,data)
                 bufwrite(l1)
-                socket.send(data)
+                sk.send(data)
     def service_actions(self):
             if not q.empty(): 
                 l=q.get()
